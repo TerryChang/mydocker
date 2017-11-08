4dc6ce85034ba2a3dbe175fcada01a99a01cc42d
Terry Chang <Terry Chang@TerryNote>
2017-11-09 01:16:27 +0900

    oracle 삭제

--- centosprod_oracle/resources/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# 부팅한뒤 추가로 구동해야 할 shell script를 여기에 작성한다
-
-# Oracle listener 설정과 관련된 내용을 여기에 기술한다
-/u01/app/oracle/product/11.2.0/xe/network/admin/listener_setup.sh
-
-exec /usr/sbin/init
