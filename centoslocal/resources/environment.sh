038b77a54aba265609cffdf937967230cdb19cc3
Terry Chang <furywolf@hanmail.net>
2017-11-10 19:05:22 +0900

    Maven 설치, Locale 설정 재구성, 디렉토리 재구성

---
+++ centoslocal/resources/environment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+JAVA_HOME=/opt/java
+MAVEN_HOME=/opt/maven
+PATH=$PATH:$JAVA_HOME/bin:$MAVEN_HOME/bin
+LANG=ko_KR.utf8
+
+export JAVA_HOME
+export MAVEN_HOME
+export LANG
+export PATH
